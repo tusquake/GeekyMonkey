86ee002f0cc79409b38a976eea55b23eb8410ace
Tushar Seth <77339749+tusquake@users.noreply.github.com>
2023-08-22 14:02:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/footer.scss";
+import "./styles/mediaquery.scss";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>GeekyMonkey</h1>
+        <p>&#169; right reserved by <span>Tushar🚀</span></p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/@stillcompiling7540/about" target={"_blank"}>
+            Youtube
+          </a>
+          <a href="https://www.linkedin.com/in/tushar-seth-4241561bb/" target={"_blank"}>
+            Linkedin
+          </a>
+          <a href="https://github.com/tusquake" target={"_blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
