415df230be871fb26495cc5c21a59133af48f281
Tushar Seth <77339749+tusquake@users.noreply.github.com>
2023-09-06 12:46:13 +0530

    Add files via upload

---
+++ src/components/Contact.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+const Contact = () => {
+
+  const [userData, setUserData] = useState({
+    Name: "",
+    Email: "",
+    Message: "",
+  });
+
+  let name, value;
+  const postUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  // connect with firebase
+  const submitData = async (event) => {
+    event.preventDefault();
+    const { Name, Email, Message} = userData;
+
+    if (Name && Email && Message) {
+      const res = await fetch(
+        "https://contactform-3fc09-default-rtdb.firebaseio.com/userDataRecords.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            Name,
+            Email,
+            Message,
+          }),
+        }
+      );
+
+
+      if (res) {
+        setUserData({
+          Name: "",
+          Email: "",
+          Message: "",
+        });
+        alert("Data Stored");
+      } else {
+        alert("Please fill the data");
+      }
+    } else {
+      alert("Please fill the data");
+    }
+  };
+
+
+  return (
+    <div className="contact">
+      <main>
+        <h1>Contact Us</h1>
+
+        <form method="POST">
+          <div>
+            <label>Name</label>
+            <input type="text" name="Name" required placeholder="Enter your Name" id="name" value={userData ? userData.Name : ''} onChange={postUserData}/>
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="email" name="Email" required placeholder="Enter your Email" id="email" value={userData ? userData.Email : ''} onChange={postUserData}/>
+          </div>
+          <div>
+            <label>Message</label>
+            <input
+              type="text"
+              name="Message"
+              required
+              placeholder="Tell us about your query..."
+              id="message"
+              // className="form-control"
+              value={userData ? userData.Message : ''}
+              onChange={postUserData}
+            />
+          </div>
+
+          <button type="submit" onClick={submitData}>Submit</button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Contact;
